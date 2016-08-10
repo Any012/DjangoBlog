f89ca93d6471f007b369f11b8b513a87349db2c9
Any012 <any012@126.com>
2016-08-10 13:13:21 +0800

    第一次上传

---
+++ my_blog/article/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length = 100)
+    category = models.CharField(max_length = 50, blank = True)
+    date_time = models.DateTimeField(auto_now_add = True)
+    content = models.TextField(blank = True, null = True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id':self.id})
+        return "http://127.0.0.1:8000%s" % path
+
+    class Meta:
+        ordering = ['-date_time']
